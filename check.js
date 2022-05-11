a2fa0822efc1bb8ae64aa6d6bf9c36499e5e926b
alexmuniz96 <alexmuniz096@gmail.com>
2022-05-11 13:52:55 -0300

    feat: validator working

---
+++ check.js
@@ -0,0 +1,11 @@
+// var soma = 0;
+// for (var i = 10; i > 1; i--) {
+//   soma += numeros.charAt(10 - i) * i;
+// }
+
+var cpf = 24985663876
+
+var final = 293
+
+
+
--- script.js
+++ script.js
@@ -1,11 +1,53 @@
-console.log('conectado')
+//cpf verificando a quantidade de digitos
 
 function validaCpf(cpf) {
-  return false
+  console.log(cpf.length)
+  if (cpf.length !== 11) {
+    return false
+  } else {
+
+    var numeros = cpf.substring(0, 9);
+    var digitos = cpf.substring(9);
+
+    var soma = 0;
+    for (var i = 10; i > 1; i--) {
+      soma += numeros.charAt(10 - i) * i;
+    }
+    console.log(soma)
+
+    // console.log(11 - (soma % 11))
+
+    var resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
+
+    //Validação do primeiro digito
+    if (resultado != digitos.charAt(0)) {
+      return false
+    }
+    // resetando a variavel para fazer a verificacao do 2 digito
+    soma = 0
+    numeros = cpf.substring(0, 10);
+
+    for (var k = 11; k > 1; k--) {
+      soma += numeros.charAt(11 - k) * k;
+    }
+
+    resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
+
+    //validação do 2 digito
+
+    if (resultado != digitos.charAt(1)) {
+      return false
+    }
+
+    return true
+  }
 }
 
 function validacao() {
   console.log('cpf avaliado');
+  document.getElementById('sucess').style.display = 'none'
+  document.getElementById('failed').style.display = 'none'
+
   var cpf = document.getElementById('cpf_digitado').value;
 
   var resutadoValidacacao = validaCpf(cpf);
